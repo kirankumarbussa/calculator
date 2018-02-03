bbbb7e2691cfc0409b007a54c53dcf63045192ca
Kiran Bussa <bussasunny72@gmail.com>
2018-02-03 12:28:02 +0530

    calculaor add functionality to project

---
+++ src/Calculator.java
@@ -0,0 +1,13 @@
+public class Calculator {
+	public static void main(String[] args) {
+		int result = addition(10,12);
+		System.out.println(result);
+	}
+
+        public static int addition(int num1, int num2){
+		return num1 + num2;
+
+    }
+}
+
+
